916a91868eb4a5cae6a240603964ed58fa3d3916
FurWaz <fur.waz06@gmail.com>
2021-03-05 17:56:22 +0100

    added multi page support
    with animations, etc

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var animationsIn = ["spawn-in-left", "spawn-in-right", "spawn-in-top", "spawn-in-bottom"];
+var animationsOut = ["spawn-out-left", "spawn-out-right", "spawn-out-top", "spawn-out-bottom"];
 const PAGES = {
     HOME: 1,
     VIDEOS: 2,
@@ -9,8 +11,14 @@ const PAGES = {
 window.onload = () => {
     var url = window.location.href;
     var pageID = url.split("page=")[1];
-    loadPage(stringToID(pageID))
+    loadPage(stringToID(pageID));
 }
+
+window.onpopstate = function(event) {
+    var url = window.location.href;
+    var pageID = url.split("page=")[1];
+    loadPage(stringToID(pageID), false);
+};
 
 /**
  * Convert any string to a page ID
@@ -72,8 +80,62 @@ function IDtoString(pageID=PAGES.HOME, capitalize=false) {
  * Loads the required web page
  * @param {string} pageID 
  */
-function loadPage(pageID=undefined) {
+function loadPage(pageID=undefined, pushState=true) {
     if (pageID == undefined) pageID = PAGES.HOME;
-    window.history.pushState({}, 'FurWaz | WebSite', '/index.html?page='+IDtoString(pageID))
+    if (pushState) window.history.pushState({}, 'FurWaz | WebSite', '/index.html?page='+IDtoString(pageID))
     document.title = "FurWaz | "+IDtoString(pageID, true);
+    var content = document.getElementById("page-content");
+    if (pageID == PAGES.HOME) {
+        document.getElementById("promo-container").style.height = "50vh";
+        document.getElementById("promo-title").style.marginTop = "14vh";
+        document.getElementById("promo-infos-back").style.transform = "translate(calc(-70vh - 8vw), 0vh) skew(30deg)";
+        document.getElementById("promo-infos-front").style.top = "17vh";
+    }
+    else {
+        document.getElementById("promo-container").style.height = "30vh";
+        document.getElementById("promo-title").style.marginTop = "2.5vh";
+        document.getElementById("promo-infos-back").style.transform = "translate(calc(-70vh - 8vw), -10vh) skew(30deg)";
+        document.getElementById("promo-infos-front").style.top = "6.5vh";
+    }
+    clearPage(content);
+    setTimeout(() => {
+        switch (pageID) {
+            case PAGES.HOME:
+                loadHomePage(content);
+                break;
+            case PAGES.VIDEOS:
+                loadVideosPage(content);
+                break;
+            case PAGES.SONGS:
+                loadSongsPage(content);
+                break;
+            case PAGES.PROJECTS:
+                loadProjectsPage(content);
+                break;
+            case PAGES.ABOUT:
+                loadAboutPage(content);
+                break;
+    
+            default:
+                break;
+        }
+    }, 250);
 }
+
+/**
+ * Clear the content in the given element
+ * @param {HTMLDivElement} content
+ */
+function clearPage(content) {
+    for (let i = 0; i < content.childElementCount; i++) {
+        content.children[i].classList.add(getRandomAnimationOut());
+    }
+    setTimeout(() => {
+        while (content.firstChild) {
+            content.firstChild.remove();
+        }
+    }, 200);
+}
+
+function getRandomAnimationIn() {return animationsIn[Math.round(Math.random()*(animationsIn.length-1))];}
+function getRandomAnimationOut() {return animationsOut[Math.round(Math.random()*(animationsOut.length-1))];}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 /**@type {http.Server} */
 var server = http.createServer( (req, res) => {
+    if (req.url == "/") req.url = "/index.html";
     if (req.url.startsWith("/index.html")) {
         res.end(fs.readFileSync("./index.html"));
         return

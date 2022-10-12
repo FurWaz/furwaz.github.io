8b8027bade8e7cbda1abcb592f4decde4fc264d5
FurWaz <fur.waz06@gmail.com>
2022-10-12 21:36:32 +0200

    Replaced website with redirection

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+# FurWaz - Website
+This website is actually just a redirection to [furwaz.com](https://furwaz.com)
--- script.js
+++
@@ -1,6 +0,0 @@
-import { generateCodeLines, InitHeader } from "./script/header.js";
-
-window.onload = () => {
-    generateCodeLines();
-    InitHeader(document.getElementById("options-container"));
-}
